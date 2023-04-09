a7a8765a5c5fde34387a0461c47087365b7a1626
niktahajighafouri <niktahajighafouri@gmail.com>
2023-04-09 17:30:06 +0430

    prodcuts view and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic import DetailView, ListView, DetailView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from .models import Category, Product
+# from utils import IsAdminUserMixing
+# from orders.forms import CartAddForm
+
+# Create your views here.
+class ProductView(View):
+    template_name = 'products/products.html'
+
+    def get(self, request, category_slug=None):
+        products = Product.objects.filter(available=True)
+        categories = Category.objects.filter(is_sub=False)
+
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        context = {
+            'products': products,
+            'categories': categories,
+        }
+        return render(request, self.template_name, context)
+
+
+class ProductDetailView(DetailView):
+    template_name = 'products/product_details.html'
+    # form_class = CartAddForm
+    model = Product
+    context_object_name = 'product'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['form'] = self.form_class()
+    #     return context
